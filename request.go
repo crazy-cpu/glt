5fe24832099f1518ec45fc12a69914ceb1dffd2e
programmer-liu <764535850@qq.com>
2022-02-23 11:12:17 +0800

    feature: DLT2007 frame constructor

--- request.go
+++ request.go
@@ -1,26 +1,26 @@
 package glt
 
 import (
+	"encoding/hex"
 	"fmt"
 )
 
-func RequestFrameAssemble2007(address string, dataMarker string) []byte {
-	var frame []byte
+func DLT2007(address string, dataMarker string) []byte {
+	var frame = make([]byte, 0)
 	addr, _ := BytesReverse(address)
 	marker, _ := BytesReverse(dataMarker)
-	fmt.Println("addr:", addr, " market:", marker)
 	frame = append(frame, []byte{0xFE, 0xFE, 0xFE, 0xFE}...) //前置唤醒
 	frame = append(frame, 0x68)                              //起始符
 	frame = append(frame, addr...)                           //地址域
 	frame = append(frame, 0x68)                              //起始符
 	frame = append(frame, 0x11)                              //控制码
 	frame = append(frame, 0x04)                              //数据长度域
-	frame = append(frame, marker...)                         //数据域
-	sum := CheckSum(frame[:18])
+	frame = append(frame, Add33H(marker)...)                 //数据域
+
+	sum := CheckSum(frame[4:18])
 
 	frame = append(frame, sum)  //校验码
 	frame = append(frame, 0x16) //结束符
-
 	return frame
 }
 
@@ -28,13 +28,12 @@ func BytesReverse(str string) ([]byte, error) {
 	if len(str)%2 != 0 {
 		return nil, fmt.Errorf("invalid hex str")
 	}
-
-	raw := []rune(str)
-
-	for i, j := 0, len(str)-2; i < j; i, j = i+2, j-2 {
-		raw[i], raw[i+1], raw[j], raw[j+1] = raw[j], raw[j+1], raw[i], raw[i+1]
+	raw, _ := hex.DecodeString(str)
+	for i, j := 0, len(raw)-1; i <= j; i, j = i+1, j-1 {
+		raw[i], raw[j] = raw[j], raw[i]
 	}
-	return []byte(string(raw)), nil
+
+	return raw, nil
 }
 
 func CheckSum(bytes []byte) byte {
@@ -43,4 +42,12 @@ func CheckSum(bytes []byte) byte {
 		sum = sum + (b % 0xFF)
 	}
 	return sum
+}
+
+func Add33H(marker []byte) []byte {
+	for i, _ := range marker {
+		marker[i] += 0x33
+	}
+
+	return marker
 }
