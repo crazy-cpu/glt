f7240f00b9f60684d6a6c004817e27fe61906a39
programmer-liu <764535850@qq.com>
2022-02-23 11:15:16 +0800

    add  test case

--- request_test.go
+++ request_test.go
@@ -1,23 +1,25 @@
 package glt
 
 import (
+	"encoding/hex"
 	"fmt"
 	"testing"
 )
 
 func TestBytesReverse(t *testing.T) {
-	str := "1A2B3C4D5E6F"
+	str := "02010100"
 	res, err := BytesReverse(str)
 	if err != nil {
 		t.Errorf(err.Error())
 	}
-	fmt.Println(string(res))
+	fmt.Println(res)
 }
 
-func TestCheckSum(t *testing.T) {
-	fmt.Println(CheckSum([]byte{0x68,0x23,0x64,0x03,0x12,0x03,0x21,0x68,0x11,0x04,0x33,0x34,0x34,0x35}))
-	r := RequestFrameAssemble2007("210312036423", "02010100")
-	fmt.Println(r)
+func TestDLT2007(t *testing.T) {
+	r := DLT2007("210312036423", "02010100")
+	fmt.Println("DLT2007 frame", hex.EncodeToString(r))
 }
 
-
+func TestCheckSum(t *testing.T){
+	
+}
